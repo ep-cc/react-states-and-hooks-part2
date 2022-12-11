dd48f65e04ceae1104aa29d5e3707e002ae2b011
Peter Erben <erben.peter@gmail.com>
2022-12-11 20:54:05 +0100

    starter component, single fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch('/api/getNames/1');
+      const data = await response.json();
+      setData(data);
+    }
+
+    load();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Movies and actors</h1>
+      { data === null &&
+        <div> Loading page </div>
+      }
+      { data &&
+        <div>{JSON.stringify(data)}</div>
+      }
     </div>
   );
 }
